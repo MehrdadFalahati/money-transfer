df6131857f25f095b108699fa10b4d6557f75c74
MehrdadFalahati <m.falahati2022@gmail.com>
2023-05-07 22:21:13 +0430

    feat: implemented adapter account repository

--- dataaccess/src/main/java/com/github/mehrdad/falahati/money/transfer/dataaccess/adapter/AccountRepositoryImpl.java
+++ dataaccess/src/main/java/com/github/mehrdad/falahati/money/transfer/dataaccess/adapter/AccountRepositoryImpl.java
@@ -1,15 +1,22 @@
 package com.github.mehrdad.falahati.money.transfer.dataaccess.adapter;
 
+import com.github.mehrdad.falahati.money.transfer.dataaccess.mapper.AccountDataAccessMapper;
+import com.github.mehrdad.falahati.money.transfer.dataaccess.repository.AccountJpaRepository;
 import com.github.mehrdad.falahati.money.transfer.domain.entity.Account;
 import com.github.mehrdad.falahati.money.transfer.domain.port.output.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
+@RequiredArgsConstructor
 public class AccountRepositoryImpl implements AccountRepository {
+
+    private final AccountJpaRepository accountJpaRepository;
+    private final AccountDataAccessMapper accountDataAccessMapper;
     @Override
     public Optional<Account> findByAccountNumber(String accountNumber) {
-        return Optional.empty();
+        return accountJpaRepository.findByAccountNumber(accountNumber).map(accountDataAccessMapper::entityToAccount);
     }
 }
