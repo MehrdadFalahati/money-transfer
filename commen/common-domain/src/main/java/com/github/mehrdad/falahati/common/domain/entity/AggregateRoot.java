22cb1f6b93551f8e5e49f52dd74a3757a3851ad2
MehrdadFalahati <m.falahati2022@gmail.com>
2023-05-07 15:00:42 +0430

    feat: implemented common-domain module

---
+++ commen/common-domain/src/main/java/com/github/mehrdad/falahati/common/domain/entity/AggregateRoot.java
@@ -0,0 +1,4 @@
+package com.github.mehrdad.falahati.common.domain.entity;
+
+public abstract class AggregateRoot<ID> extends BaseEntity<ID> {
+}
---
+++ commen/common-domain/src/main/java/com/github/mehrdad/falahati/common/domain/event/DomainEvent.java
@@ -0,0 +1,4 @@
+package com.github.mehrdad.falahati.common.domain.event;
+
+public interface DomainEvent<T> {
+}
