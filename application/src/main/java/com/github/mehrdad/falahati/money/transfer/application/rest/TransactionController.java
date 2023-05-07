4214151ae3f83aa29dd50042f33b3e8b09a95ba2
MehrdadFalahati <m.falahati2022@gmail.com>
2023-05-07 23:53:03 +0430

    feat: implemented transaction rest api

---
+++ application/src/main/java/com/github/mehrdad/falahati/money/transfer/application/rest/TransactionController.java
@@ -0,0 +1,36 @@
+package com.github.mehrdad.falahati.money.transfer.application.rest;
+
+import com.github.mehrdad.falahati.money.transfer.domain.dto.transaction.CreateTransactionCommand;
+import com.github.mehrdad.falahati.money.transfer.domain.dto.transaction.CreateTransactionResponse;
+import com.github.mehrdad.falahati.money.transfer.domain.dto.transaction.ReportTransactionQuery;
+import com.github.mehrdad.falahati.money.transfer.domain.dto.transaction.ReportTransactionResponse;
+import com.github.mehrdad.falahati.money.transfer.domain.port.input.service.TransactionHistoryApplicationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/transactions")
+@RequiredArgsConstructor
+public class TransactionController {
+
+    private final TransactionHistoryApplicationService transactionHistoryApplicationService;
+
+    @PostMapping
+    public ResponseEntity<CreateTransactionResponse> createTransaction(@RequestBody CreateTransactionCommand createTransactionCommand) {
+        log.info("Transfer money between Account[accountNumber={}] to Account[accountNumber={}]",
+                createTransactionCommand.fromAccountNumber(), createTransactionCommand.toAccountNumber());
+        CreateTransactionResponse createTransactionResponse = transactionHistoryApplicationService.createTransaction(createTransactionCommand);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createTransactionResponse);
+    }
+
+    @GetMapping("/reports")
+    public ResponseEntity<ReportTransactionResponse> reports(@RequestBody ReportTransactionQuery reportTransactionQuery) {
+        log.info("Transaction report for Account[accountNumber={}]", reportTransactionQuery.accountNumber());
+        ReportTransactionResponse reportTransactionResponse = transactionHistoryApplicationService.reportTransactions(reportTransactionQuery);
+        return ResponseEntity.ok(reportTransactionResponse);
+    }
+}
